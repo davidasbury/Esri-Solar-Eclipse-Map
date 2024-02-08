77053eb5a536ba02ac9f02edb1595d43806fd309
David Asbury <DAsbury@esri.com>
2024-02-08 15:52:38 -0800

    fixed goTo() features that cross the 180 discontinuity

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ require([
   "esri/Map",
   "esri/Graphic",
   "esri/geometry/SpatialReference",
+  "esri/geometry/Extent",
   "esri/symbols/FillSymbol3DLayer",
   "esri/symbols/PolygonSymbol3D",
   "esri/renderers/SimpleRenderer",
@@ -35,6 +36,7 @@ require([
   Map,
   Graphic,
   SpatialReference,
+  Extent,
   FillSymbol3DLayer,
   PolygonSymbol3D,
   SimpleRenderer,
@@ -637,11 +639,29 @@ require([
           _currentTime = date.getFullYear();
 
           // Pan to selected graphic
-          _view.goTo({
-            target: d,
-            heading: 0,
-          });
+          if (d.geometry.extent.center.longitude > 0.5) {
+            var new_center = [
+              d.geometry.extent.center.longitude,
+              d.geometry.extent.center.latitude,
+            ];
+          } else if (d.geometry.extent.center.longitude < 0) {
+            var new_center = [
+              d.geometry.extent.center.longitude + 359,
+              d.geometry.extent.center.latitude,
+            ];
+          }
+          // Deal with features that cross the 180º discontinuity
+          else {
+            var new_center = [179.9, d.geometry.extent.center.latitude];
+          }
 
+          _view.goTo({ center: new_center });
+
+          console.log(
+            "view_center: ",
+            _view.center.latitude,
+            _view.center.longitude
+          );
           // Move time pointer
           movePointer();
 
